54aab25309b456bf90146a3be63554eac6f1cf81
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2019-08-01 10:19:25 -0500

    adds components

---
+++ src/shared/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>Hello World</div>  
+    );
+  }
+}
+
+export default Nav
